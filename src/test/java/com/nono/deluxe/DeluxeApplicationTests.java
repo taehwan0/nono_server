ef5a927290e917331ccd8c7d22e9f543a422ab21
buddy <81221429+taehwan0@users.noreply.github.com>
2022-06-17 12:52:15 +0900

    Project Created

---
+++ src/test/java/com/nono/deluxe/DeluxeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nono.deluxe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeluxeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
