cdfbf0236937b7002fdc2c94372fec8531e4be6c
buddy <81221429+taehwan0@users.noreply.github.com>
2022-06-21 10:08:36 +0900

    Update Notice API & Test DB

--- src/main/java/com/nono/deluxe/controller/notice/dto/CreateNoticeRequestDto.java
+++ src/main/java/com/nono/deluxe/controller/notice/dto/CreateNoticeRequestDto.java
@@ -1,10 +1,13 @@
 package com.nono.deluxe.controller.notice.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 import org.springframework.web.bind.annotation.GetMapping;
 
-@Getter
+@Getter @Setter
+@AllArgsConstructor
 @NoArgsConstructor
 public class CreateNoticeRequestDto {
     private String writer;
--- src/test/java/com/nono/deluxe/controller/notice/NoticeControllerTest.java
+++ src/test/java/com/nono/deluxe/controller/notice/NoticeControllerTest.java
@@ -1,5 +1,7 @@
 package com.nono.deluxe.controller.notice;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.nono.deluxe.controller.notice.dto.CreateNoticeRequestDto;
 import com.nono.deluxe.domain.notice.NoticeRepository;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
@@ -8,9 +10,18 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.data.repository.query.Param;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.RequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.result.StatusResultMatchers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class NoticeControllerTest {
@@ -19,6 +30,9 @@ class NoticeControllerTest {
     private int port;
 
     private MockMvc mvc;
+
+    @Autowired
+    ObjectMapper objectMapper;
 
     @Autowired
     WebApplicationContext context;
@@ -33,16 +47,34 @@ class NoticeControllerTest {
                 .build();
     }
 
-    @AfterEach
-    public void after() {
-        // 연결된 DB 지워짐. 테스트용 h2를 쓰던가 하는 방법이 필요할듯.
-        // Too many connections 처리에 대한 대책 강구.
-        noticeRepository.deleteAll();
-    }
+//    @AfterEach
+//    public void after() {
+//        // 연결된 DB 지워짐. 테스트용 h2를 쓰던가 하는 방법이 필요할듯.
+//        // Too many connections 처리에 대한 대책 강구.
+//        noticeRepository.deleteAll();
+//    }
 
     @Test
-    public void test() {
-        System.out.println("hello");
+    public void createNotice() throws Exception {
+        //given
+        CreateNoticeRequestDto createNoticeRequestDto = new CreateNoticeRequestDto(
+                "writer",
+                "title",
+                "한글도 잘 들어가나요",
+                true
+        );
+        String content = objectMapper.writeValueAsString(createNoticeRequestDto);
+
+        //when
+        mvc.perform(
+                post("/notice", createNoticeRequestDto)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(content))
+                .andExpect(status().isOk())
+                .andDo(print());
+
+        //then
+
     }
 
 
