6fbdd5ec2ce26b562ec28e5e0f2233784a28cda8
buddy <81221429+taehwan0@users.noreply.github.com>
2022-09-17 23:45:36 +0900

    feat: Auth add controller template

--- src/main/java/com/nono/deluxe/controller/AuthController.java
+++ src/main/java/com/nono/deluxe/controller/AuthController.java
@@ -3,6 +3,7 @@ package com.nono.deluxe.controller;
 import com.nono.deluxe.controller.dto.auth.LoginRequestDTO;
 import com.nono.deluxe.service.AuthService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,5 +22,33 @@ public class AuthController {
                 loginRequestDto.getPassword(),
                 loginRequestDto.getTokenActiveSeconds()
         );
+    }
+
+    @PostMapping("/join")
+    public void joinUser() {
+    }
+
+    @PostMapping("/email/duplicate")
+    public void checkDuplicateEmail() {
+    }
+
+    @PostMapping("/email/check")
+    public void postCheckEmail() {
+    }
+
+    @PostMapping("/email/verify")
+    public void verifyEmail() {
+    }
+
+    @PostMapping("/reissue")
+    public void reissueUser() {
+    }
+
+    @PostMapping("/logincode")
+    public void getLoginCode() {
+    }
+
+    @PostMapping("/logincode/verify")
+    public void verifyLoginCode() {
     }
 }
