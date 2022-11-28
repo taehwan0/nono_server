1c04b325a1c92e252665be46178008c68261ae44
buddy <jang_taehwan@naver.com>
2022-11-28 16:45:30 +0900

    feat(ImageFile): 이미지 변환 및 S3 업로드 기능 구현

--- src/main/java/com/nono/deluxe/service/FileService.java
+++ src/main/java/com/nono/deluxe/service/FileService.java
@@ -5,9 +5,11 @@ import com.amazonaws.services.s3.model.ObjectMetadata;
 import com.nono.deluxe.controller.dto.imagefile.UploadImageFileResponseDTO;
 import com.nono.deluxe.domain.imagefile.ImageFile;
 import com.nono.deluxe.domain.imagefile.ImageFileRepository;
+import java.io.File;
 import java.io.IOException;
 import java.util.UUID;
 import lombok.RequiredArgsConstructor;
+import net.coobird.thumbnailator.Thumbnails;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
@@ -16,6 +18,12 @@ import org.springframework.web.multipart.MultipartFile;
 @Service
 public class FileService {
 
+    private static final String SUFFIX = ".png";
+    private static final String THUMBNAIL_TAIL = "-th";
+    private static final int IMAGE_WIDTH = 1000;
+    private static final int IMAGE_HEIGHT = 1000;
+    private static final double OUTPUT_QUALITY = 0.7;
+
     private final ImageFileRepository imageFileRepository;
     private final AmazonS3Client amazonS3Client;
 
@@ -23,18 +31,13 @@ public class FileService {
     private String bucket;
 
     public UploadImageFileResponseDTO uploadImageFile(MultipartFile imageFile) throws IOException {
-        // 1. 이미지 파일을 받아서
-        // 2. UUID 2개 생성
-        // 2-1. 오리지널 이미지 저장 및 url 반환
-        // 2-2. 섬네일 이미지 저장 및 url 반환
-        // 3. entity 만들고 dto 로 반환
-
-        String originalUrl = putS3(imageFile);
-//        String thumbnailUrl = putS3(convertThumbnailImage(imageFile));
+        String uuid = UUID.randomUUID().toString();
+        String originalUrl = putS3(imageFile, uuid);
+        String thumbnailUrl = putS3(convertToThumbnail(imageFile), uuid);
 
         ImageFile savedImageFile = ImageFile.builder()
             .originalUrl(originalUrl)
-            .thumbnailUrl("")
+            .thumbnailUrl(thumbnailUrl)
             .build();
 
         imageFileRepository.save(savedImageFile);
@@ -42,13 +45,40 @@ public class FileService {
         return new UploadImageFileResponseDTO(savedImageFile);
     }
 
-    private String putS3(MultipartFile imageFile) throws IOException {
-        String originalImageFileName = UUID.randomUUID() + "-" + imageFile.getOriginalFilename();
+    private File convertToThumbnail(MultipartFile imageFile) throws IOException {
+        File file = File.createTempFile(imageFile.getName() + THUMBNAIL_TAIL, SUFFIX);
+        Thumbnails.of(imageFile.getInputStream())
+            .size(IMAGE_WIDTH, IMAGE_HEIGHT)
+            .outputQuality(OUTPUT_QUALITY)
+            .toFile(file);
+
+        return file;
+    }
+
+    private String putS3(MultipartFile imageFile, String fileName) throws IOException {
+        fileName += SUFFIX;
 
         ObjectMetadata objectMetadata = new ObjectMetadata();
         objectMetadata.setContentLength(imageFile.getInputStream().available());
+        objectMetadata.setContentType("image/png");
 
-        amazonS3Client.putObject(bucket, originalImageFileName, imageFile.getInputStream(), objectMetadata);
-        return amazonS3Client.getUrl(bucket, originalImageFileName).toString();
+        amazonS3Client.putObject(bucket, fileName, imageFile.getInputStream(), objectMetadata);
+
+        return amazonS3Client.getUrl(bucket, fileName).toString();
+    }
+
+    private String putS3(File file, String fileName) {
+        fileName += THUMBNAIL_TAIL + SUFFIX;
+
+        amazonS3Client.putObject(bucket, fileName, file);
+        deleteFile(file);
+
+        return amazonS3Client.getUrl(bucket, fileName).toString();
+    }
+
+    private void deleteFile(File file) {
+        if (!file.delete()) {
+            throw new RuntimeException("썸네일 파일 제거에 실패했습니다.");
+        }
     }
 }
