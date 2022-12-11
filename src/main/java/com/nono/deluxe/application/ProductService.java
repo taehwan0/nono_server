e1b6b19fc600d967c77ae4fb7b7fee3ca44fc828
buddy <jang_taehwan@naver.com>
2022-12-11 22:03:43 +0900

    refactor(Product): Product Service 로직 최적화

--- src/main/java/com/nono/deluxe/application/ProductService.java
+++ src/main/java/com/nono/deluxe/application/ProductService.java
@@ -1,7 +1,6 @@
 package com.nono.deluxe.application;
 
 import com.amazonaws.services.kms.model.NotFoundException;
-import com.nono.deluxe.domain.imagefile.ImageFile;
 import com.nono.deluxe.domain.imagefile.ImageFileRepository;
 import com.nono.deluxe.domain.product.Product;
 import com.nono.deluxe.domain.product.ProductRepository;
@@ -13,20 +12,16 @@ import com.nono.deluxe.presentation.dto.product.GetProductListResponseDTO;
 import com.nono.deluxe.presentation.dto.product.GetRecordListResponseDTO;
 import com.nono.deluxe.presentation.dto.product.ProductResponseDTO;
 import com.nono.deluxe.presentation.dto.product.UpdateProductRequestDTO;
+import com.nono.deluxe.utils.LocalDateCreator;
 import java.time.LocalDate;
 import java.util.List;
-import java.util.Locale;
 import javax.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
-/**
- * Pageable 적용 필요 연도, 월별 record 불러오기의 날짜 비교 또한 쿼리로 처리할 예정
- */
 @RequiredArgsConstructor
 @Service
 public class ProductService {
@@ -38,32 +33,29 @@ public class ProductService {
     @Transactional
     public ProductResponseDTO createProduct(CreateProductRequestDto requestDto) {
         Product product = requestDto.toEntity();
-        if (requestDto.getImageFileId() != null) {
-            ImageFile imageFile = imageFileRepository.findById(requestDto.getImageFileId())
-                .orElseThrow(() -> new NotFoundException("Not exist ImageFile"));
 
-            product.updateImageFile(imageFile);
+        if (requestDto.getImageFileId() != null) {
+            imageFileRepository.findById(requestDto.getImageFileId())
+                .ifPresent(product::updateImageFile);
         }
 
         return new ProductResponseDTO(productRepository.save(product));
     }
 
-    /**
-     * from.hj.yang 해당 부분 Pageable 사용하여 변경 완료.
-     */
-    public GetProductListResponseDTO getProductList(String query, String column, String order, int size, int page,
+    public GetProductListResponseDTO getProductList(
+        String query,
+        String column,
+        String order,
+        int size,
+        int page,
         boolean active) {
-        Pageable pageable = PageRequest.of(page, size,
-            Sort.by(new Sort.Order(Sort.Direction.valueOf(order.toUpperCase(Locale.ROOT)), column)));
-        Page<Product> productPage;
+        Pageable pageable =
+            PageRequest.of(page, size, Sort.by(new Sort.Order(Sort.Direction.valueOf(order.toUpperCase()), column)));
 
         if (active) {
-            productPage = productRepository.readActiveProductList(query, pageable); // true -> active 만 읽기
-        } else {
-            productPage = productRepository.readProductList(query, pageable); // false -> 전체 읽기
+            return new GetProductListResponseDTO(productRepository.readActiveProductList(query, pageable));
         }
-
-        return new GetProductListResponseDTO(productPage);
+        return new GetProductListResponseDTO(productRepository.readProductList(query, pageable));
     }
 
     public ProductResponseDTO getProductInfo(long productId) {
@@ -80,24 +72,13 @@ public class ProductService {
         return new ProductResponseDTO(product);
     }
 
-    /**
-     * from.hj.yang 1. 해당 부분에 대하여 Query만으로 해당 부분의 데이터를 추출하여 처리하는 부분에 대한 공부 필요.
-     */
     public GetRecordListResponseDTO readProductRecord(long productId, int year, int month) {
-        if (year == 0) {
-            year = LocalDate.now().getYear();
-        }
-        int toMonth = month;
-        if (month == 0) {
-            month = 1;
-            toMonth = 12;
-        }
-
-        LocalDate fromDate = LocalDate.of(year, month, 1);
-        LocalDate toDate = LocalDate.of(year, toMonth, LocalDate.of(year, toMonth, 1).lengthOfMonth());
+        LocalDate fromDate = LocalDateCreator.getFromDate(year, month);
+        LocalDate toDate = LocalDateCreator.getToDate(year, month);
 
         Product product = productRepository.findById(productId)
             .orElseThrow(() -> new RuntimeException("Not Exist Product."));
+
         List<Record> recordList = recordRepository.findByProductId(productId, fromDate, toDate);
 
         return new GetRecordListResponseDTO(product, recordList);
@@ -111,9 +92,8 @@ public class ProductService {
         updatedProduct.updateInfo(requestDTO);
 
         if (requestDTO.getImageFileId() != null) {
-            ImageFile newImageFile = imageFileRepository.findById(requestDTO.getImageFileId())
-                .orElseThrow(() -> new NotFoundException("Not Exist Image"));
-            updatedProduct.updateImageFile(newImageFile);
+            imageFileRepository.findById(requestDTO.getImageFileId())
+                .ifPresent(updatedProduct::updateImageFile);
         }
 
         productRepository.save(updatedProduct);
@@ -123,9 +103,8 @@ public class ProductService {
 
     @Transactional
     public MessageResponseDTO deleteProduct(long productId) {
-        Product product = productRepository.findById(productId)
-            .orElseThrow(() -> new NotFoundException("Product: not found id"));
-        product.delete();
+        productRepository.findById(productId)
+            .ifPresent(Product::delete);
 
         return new MessageResponseDTO(true, "deleted");
     }
